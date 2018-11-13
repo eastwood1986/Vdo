a9a4a4e19c4013bd2ccf2004f3d0556e17ad3150
eastwood <miaorenjun0325@yahoo.co.jp>
2018-11-13 18:38:56 +0900

    add server

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
-var express = require('express');
-var app = express();
+// expressの呼び込み
+const express = require('express');
+// PORTの設定
+// もしenvにportが確立したらそのポートを、していなかったら8080を使う
+const port = process.env.PORT || 5000;
+// express()のインスタンス
+const app = express();
 
-var user = process.env.USER;
-var pass = process.env.PASS;
+// どこにアクセスさせるかの設定
+// distフォルダ：コンパイルされたファイルが入る場所
+app.use(express.static(__dirname + "/dist/"));
 
-app.set('port', process.env.PORT || 3000);
-
-if (user && pass) {
-  app.use(express.basicAuth(user, pass));
-}
-
-app.use(express.logger('dev'));
-app.use(express.compress());
-app.use(express.static(__dirname));
-
-app.listen(app.get('port'), function() {
-  console.log('Server listening on port %s', app.get('port'));
+// SPAの時、ルーティングがうまくいかない時があるので以下の設定
+// これをしていないと、https://~~~/aboutとかに行った時にリロードするとエラーがでる
+// SPAだとaboutファイルを直で読み込んでいないから
+// これでルート以外でリロードしてもindex.htmlを読み込んでちゃんとルーティングをしてくれる
+// /.*/で全てのルートを指定。req(request), res(response)
+app.get(/.*/, function(req, res) {
+    res.sendfile(__dirname + "/dist/index.html");
 });
+
+// 一番上で指定したportをlisten
+app.listen(port);
+
+// 動いてるかどうかconsoleで見る
+console.log("Server is up!!");
